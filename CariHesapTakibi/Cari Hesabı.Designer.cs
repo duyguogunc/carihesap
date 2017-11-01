c95c7e2ee03c39d6fdafe8a72002e6a4633e2915
nuuklu <nuuklu@WISSEN-PC>
2017-11-01 14:20:25 +0200

    cari hesabı detay son

--- CariHesapTakibi/Cari Hesabı.Designer.cs
+++ CariHesapTakibi/Cari Hesabı.Designer.cs
@@ -37,6 +37,15 @@
             this.label9 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.lbl_carihesap = new System.Windows.Forms.Label();
+            this.lbl_tutar = new System.Windows.Forms.Label();
+            this.lbl_islem = new System.Windows.Forms.Label();
+            this.lbl_islemtutarı = new System.Windows.Forms.Label();
+            this.lbl_vadetarihi = new System.Windows.Forms.Label();
+            this.lbl_vadegünü = new System.Windows.Forms.Label();
+            this.lbl_evrakno = new System.Windows.Forms.Label();
+            this.lbl_evraktipi = new System.Windows.Forms.Label();
+            this.lbl_evrakcinsi = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label6
@@ -87,7 +96,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 29);
+            this.label1.Location = new System.Drawing.Point(26, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(62, 13);
             this.label1.TabIndex = 28;
@@ -120,11 +129,92 @@
             this.label7.TabIndex = 34;
             this.label7.Text = "Evrak No:";
             // 
+            // lbl_carihesap
+            // 
+            this.lbl_carihesap.Location = new System.Drawing.Point(130, 28);
+            this.lbl_carihesap.Name = "lbl_carihesap";
+            this.lbl_carihesap.Size = new System.Drawing.Size(62, 20);
+            this.lbl_carihesap.TabIndex = 37;
+            this.lbl_carihesap.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_tutar
+            // 
+            this.lbl_tutar.Location = new System.Drawing.Point(130, 53);
+            this.lbl_tutar.Name = "lbl_tutar";
+            this.lbl_tutar.Size = new System.Drawing.Size(62, 20);
+            this.lbl_tutar.TabIndex = 38;
+            this.lbl_tutar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_islem
+            // 
+            this.lbl_islem.Location = new System.Drawing.Point(130, 83);
+            this.lbl_islem.Name = "lbl_islem";
+            this.lbl_islem.Size = new System.Drawing.Size(62, 20);
+            this.lbl_islem.TabIndex = 39;
+            this.lbl_islem.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_islemtutarı
+            // 
+            this.lbl_islemtutarı.Location = new System.Drawing.Point(130, 113);
+            this.lbl_islemtutarı.Name = "lbl_islemtutarı";
+            this.lbl_islemtutarı.Size = new System.Drawing.Size(62, 20);
+            this.lbl_islemtutarı.TabIndex = 40;
+            this.lbl_islemtutarı.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_vadetarihi
+            // 
+            this.lbl_vadetarihi.Location = new System.Drawing.Point(130, 149);
+            this.lbl_vadetarihi.Name = "lbl_vadetarihi";
+            this.lbl_vadetarihi.Size = new System.Drawing.Size(62, 20);
+            this.lbl_vadetarihi.TabIndex = 41;
+            this.lbl_vadetarihi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_vadegünü
+            // 
+            this.lbl_vadegünü.Location = new System.Drawing.Point(130, 178);
+            this.lbl_vadegünü.Name = "lbl_vadegünü";
+            this.lbl_vadegünü.Size = new System.Drawing.Size(62, 20);
+            this.lbl_vadegünü.TabIndex = 42;
+            this.lbl_vadegünü.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_evrakno
+            // 
+            this.lbl_evrakno.Location = new System.Drawing.Point(130, 212);
+            this.lbl_evrakno.Name = "lbl_evrakno";
+            this.lbl_evrakno.Size = new System.Drawing.Size(62, 20);
+            this.lbl_evrakno.TabIndex = 43;
+            this.lbl_evrakno.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_evraktipi
+            // 
+            this.lbl_evraktipi.Location = new System.Drawing.Point(130, 247);
+            this.lbl_evraktipi.Name = "lbl_evraktipi";
+            this.lbl_evraktipi.Size = new System.Drawing.Size(62, 20);
+            this.lbl_evraktipi.TabIndex = 44;
+            this.lbl_evraktipi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_evrakcinsi
+            // 
+            this.lbl_evrakcinsi.Location = new System.Drawing.Point(130, 280);
+            this.lbl_evrakcinsi.Name = "lbl_evrakcinsi";
+            this.lbl_evrakcinsi.Size = new System.Drawing.Size(62, 20);
+            this.lbl_evrakcinsi.TabIndex = 45;
+            this.lbl_evrakcinsi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Cari_Hesabı
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(366, 338);
+            this.Controls.Add(this.lbl_evrakcinsi);
+            this.Controls.Add(this.lbl_evraktipi);
+            this.Controls.Add(this.lbl_evrakno);
+            this.Controls.Add(this.lbl_vadegünü);
+            this.Controls.Add(this.lbl_vadetarihi);
+            this.Controls.Add(this.lbl_islemtutarı);
+            this.Controls.Add(this.lbl_islem);
+            this.Controls.Add(this.lbl_tutar);
+            this.Controls.Add(this.lbl_carihesap);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -152,5 +242,14 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbl_carihesap;
+        private System.Windows.Forms.Label lbl_tutar;
+        private System.Windows.Forms.Label lbl_islem;
+        private System.Windows.Forms.Label lbl_islemtutarı;
+        private System.Windows.Forms.Label lbl_vadetarihi;
+        private System.Windows.Forms.Label lbl_vadegünü;
+        private System.Windows.Forms.Label lbl_evrakno;
+        private System.Windows.Forms.Label lbl_evraktipi;
+        private System.Windows.Forms.Label lbl_evrakcinsi;
     }
 }
